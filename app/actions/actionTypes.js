861591d84ec1c25a792b396ee399c304c24feb1e
seawind8888 <344023858@qq.com>
2019-07-03 15:35:35 +0800

    init

--- app/actions/actionTypes.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by haifeng on 17/1/10.
- */
-export const FETCH_NEWS_INIT = 'FETCH_NEWS_INIT';
-export const FETCH_NEWS_LIST = 'FETCH_NEWS_LIST';
-export const FETCH_INVEST_INIT = 'FETCH_INVEST_INIT';
-export const FETCH_INVEST_LIST = 'FETCH_INVEST_LIST';
--- app/common/Request.js
+++
@@ -1,6 +0,0 @@
-
-
-
-export const LOGIN_ACTION = "LoginAction";  //用户登录
-
-export const REGISTER_ACTION = "RegisterAction"; //用户注册
